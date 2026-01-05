2e5ff608b95c45e89ba23dcb7f4a9ff67c54c796
Abolfazl <abolfazl.misaghii.@gmail.com>
2026-01-05 16:25:07 +0330

    start script code

---
+++ script.js
@@ -0,0 +1,7 @@
+const inputEl=document.querySelector(".input")
+const buttonAddEl=document.querySelector(".buttonAdd")
+
+function addToList(){
+    alert("*");
+}
+buttonAddEl.addEventListener("click", addToList)
